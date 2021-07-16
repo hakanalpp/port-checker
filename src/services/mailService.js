a16489a132cebe80aa4b6e3aee35dcef10b90d43
Hakan Alp <hakana926@gmail.com>
2021-07-16 20:49:29 +0300

    mail service implemented

---
+++ src/services/mailService.js
@@ -0,0 +1,28 @@
+import { createTransport } from "nodemailer";
+
+import { getDate } from "../helpers/dateHelper.js";
+
+export const sendMail = async (message) => {
+  console.log(`[${getDate()}] Sending the email with the following message: ${message}`);
+  try {
+    const transporter = createTransport({
+      host: "smtp-mail.outlook.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.SENDER_EMAIL,
+        pass: process.env.SENDER_PASSWORD
+      }
+    });
+
+    await transporter.sendMail({
+      from: process.env.SENDER_EMAIL,
+      to: process.env.RECEIVER_EMAIL,
+      subject: process.env.SUBJECT,
+      text: message
+    });
+    console.log(`[${getDate()}] Email successfully sent.`);
+  } catch (err) {
+    console.log(`[${getDate()}] ${err}`);
+  }
+};
